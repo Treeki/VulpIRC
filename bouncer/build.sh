ba93305786f0a96c4e6fcc1de10f718132f1c6e0
Treeki <treeki@gmail.com>
2014-02-25 07:08:18 +0100

    add USE_ZLIB to build scripts

--- bouncer/build.sh
+++ bouncer/build.sh
@@ -3,7 +3,7 @@ mkdir -p binary
 
 NETCODE="socketcommon.cpp client.cpp mobileclient.cpp server.cpp ircserver.cpp netcore.cpp"
 SOURCES="$NETCODE main.cpp window.cpp dns.cpp ini.cpp richtext.cpp zlibwrapper.cpp"
-FLAGS="-Wall -std=c++11 -DUSE_GNUTLS -lgnutls -lz -pthread -g"
+FLAGS="-Wall -std=c++11 -DUSE_GNUTLS -lgnutls -DUSE_ZLIB -lz -pthread -g"
 
 g++ -o binary/vulpircd $FLAGS $SOURCES
 
--- bouncer/build_static.sh
+++ bouncer/build_static.sh
@@ -3,7 +3,7 @@ mkdir -p binary
 
 NETCODE="socketcommon.cpp client.cpp mobileclient.cpp server.cpp ircserver.cpp netcore.cpp"
 SOURCES="$NETCODE main.cpp window.cpp dns.cpp ini.cpp richtext.cpp zlibwrapper.cpp"
-FLAGS="-Wall -static -static-libgcc -static-libstdc++ -std=c++11 -pthread"
+FLAGS="-Wall -static -static-libgcc -static-libstdc++ -std=c++11 -pthread -DUSE_ZLIB"
 
 g++ -o binary/vulpircd_static $FLAGS $SOURCES -lz
 
